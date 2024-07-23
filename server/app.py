f84566593d6eb0e7605bd42472cb9d409a64893e
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-07-23 16:44:06 +0300

    initial

--- server/app.py
+++ server/app.py
@@ -17,11 +17,29 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plants_list = [plant.to_dict() for plant in plants]
+        return make_response(jsonify(plants_list), 200)
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data.get('name'),
+            image=data.get('image'),
+            price=data.get('price')
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get_or_404(id)
+        return make_response(jsonify(plant.to_dict()), 200)
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
